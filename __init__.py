b986e86d84bff56d4cd57397e9838636e0a3078f
resteve <resteve@zikzakmedia.com>
2015-07-05 11:53:12 +0200

    Thumb

--- __init__.py
+++ __init__.py
@@ -1,13 +1,14 @@
 # This file is part product_attachments module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
 from trytond.pool import Pool
 from .attachment import *
+from .configuration import *
 from .product import *
 
 def register():
     Pool.register(
+        Configuration,
         Attachment,
         Template,
         Product,
--- product.py
+++ product.py
@@ -1,12 +1,16 @@
 # This file is part product_attachments module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
+import os
+import hashlib
+import magic
+from PIL import Image
+from mimetypes import guess_type
 from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
 from trytond.transaction import Transaction
 from trytond.config import config
-import magic
 
 __all__ = ['Template', 'Product']
 __metaclass__ = PoolMeta
@@ -23,6 +27,21 @@ class Template:
         'Attachments', states=STATES, depends=DEPENDS)
     image = fields.Function(fields.Char('Image'),
         'get_image')
+    thumb = fields.Function(fields.Binary('Thumb', filename='thumb_filename',
+        help='Thumbnail Product Image'), 'get_thumb', setter='set_thumb')
+    thumb_filename = fields.Char('File Name',
+        help='Thumbnail Product File Name')
+    thumb_path = fields.Function(fields.Char('Thumb Path'), 'get_thumbpath')
+
+    @classmethod
+    def __setup__(cls):
+        super(Template, cls).__setup__()
+        cls._error_messages.update({
+            'not_file_mime': ('Not know file mime "%(file_name)s"'),
+            'not_file_mime_image': ('"%(file_name)s" file mime is not an image ' \
+                '(jpg, png or gif)'),
+            'image_size': ('Thumb "%(file_name)s" size is larger than "%(size)s"Kb'),
+        })
 
     @classmethod
     def delete(cls, templates):
@@ -55,6 +74,106 @@ class Template:
             if mimetype in _IMAGE_TYPES:
                 return digest
         return
+
+
+    def get_thumb(self, name):
+        db_name = Transaction().cursor.dbname
+        filename = self.thumb_filename
+        if not filename:
+            return None
+        filename = os.path.join(config.get('database', 'path'), db_name,
+            'esale', 'thumb', filename[0:2], filename[2:4], filename)
+
+        value = None
+        try:
+            with open(filename, 'rb') as file_p:
+                value = buffer(file_p.read())
+        except IOError:
+            pass
+        return value
+
+    def get_thumbpath(self, name):
+        filename = self.thumb_filename
+        if not filename:
+            return None
+        return '%s/%s/%s' % (filename[:2], filename[2:4], filename)
+
+    @classmethod
+    def set_thumb(cls, templates, name, value):
+        if value is None:
+            return
+        if not value:
+            cls.write(templates, {
+                'thumb_filename': None,
+                })
+            return
+
+        Config = Pool().get('product.configuration')
+        product_config = Config(1)
+        size = product_config.thumb_size or 150
+
+        db_name = Transaction().cursor.dbname
+        esaledir = os.path.join(
+            config.get('database', 'path'), db_name, 'esale', 'thumb')
+
+        for template in templates:
+            file_name = template['thumb_filename']
+
+            file_mime, _ = guess_type(file_name)
+            if not file_mime:
+                cls.raise_user_error('not_file_mime', {
+                        'file_name': file_name,
+                        })
+            if file_mime not in _IMAGE_TYPES:
+                cls.raise_user_error('not_file_mime_image', {
+                        'file_name': file_name,
+                        })
+
+            _, ext = file_mime.split('/')
+            digest = '%s.%s' % (hashlib.md5(value).hexdigest(), ext)
+            subdir1 = digest[0:2]
+            subdir2 = digest[2:4]
+            directory = os.path.join(esaledir, subdir1, subdir2)
+            filename = os.path.join(directory, digest)
+
+            if not os.path.isdir(directory):
+                os.makedirs(directory, 0775)
+            os.umask(0022)
+            with open(filename, 'wb') as file_p:
+                file_p.write(value)
+
+            # square and thumbnail thumb image
+            thumb_size = size, size
+            try:
+                im = Image.open(filename)
+            except:
+                if os.path.exists(filename):
+                    os.remove(filename)
+                cls.raise_user_error('not_file_mime_image', {
+                        'file_name': file_name,
+                        })
+
+            width, height = im.size
+            if width > height:
+               delta = width - height
+               left = int(delta/2)
+               upper = 0
+               right = height + left
+               lower = height
+            else:
+               delta = height - width
+               left = 0
+               upper = int(delta/2)
+               right = width
+               lower = width + upper
+
+            im = im.crop((left, upper, right, lower))
+            im.thumbnail(thumb_size, Image.ANTIALIAS)
+            im.save(filename)
+
+            cls.write([template], {
+                'thumb_filename': digest,
+                })
 
 
 class Product:
