c52493679fcb4d8a529988e3c8ee4952fc3fc38c
jmartin <jmartin@zikzakmedia.com>
2016-01-25 15:38:35 +0100

    Remove auto uninstallation of module

--- configuration.py
+++ configuration.py
@@ -2,10 +2,7 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import Pool, PoolMeta
-from trytond.transaction import Transaction
-
-from sql import Literal
+from trytond.pool import PoolMeta
 
 
 __all__ = ['Configuration']
@@ -16,22 +13,6 @@ class Configuration:
     __name__ = 'product.configuration'
     thumb_size = fields.Integer('Thumb Size',
         help='Thumbnail Product Image Size (width and height)')
-
-    @classmethod
-    def __register__(cls, module_name):
-        pool = Pool()
-        Module = pool.get('ir.module')
-
-        cursor = Transaction().cursor
-        module = Module.__table__()
-
-        cursor.execute(*module.update(
-                columns=[module.state],
-                values=[Literal('to remove')],
-                where=module.name == Literal('product_configuration')
-                ))
-
-        super(Configuration, cls).__register__(module_name)
 
     @staticmethod
     def default_thumb_size():
