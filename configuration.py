8def363313c862c8649f916e0168ad155bbd1660
jmartin <jmartin@zikzakmedia.com>
2016-01-25 14:10:27 +0100

    Change dependency of product_configuration to product module and remove it product_configuration module when update this module

--- configuration.py
+++ configuration.py
@@ -2,7 +2,11 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
+from trytond.transaction import Transaction
+
+from sql import Literal
+
 
 __all__ = ['Configuration']
 __metaclass__ = PoolMeta
@@ -12,6 +16,22 @@ class Configuration:
     __name__ = 'product.configuration'
     thumb_size = fields.Integer('Thumb Size',
         help='Thumbnail Product Image Size (width and height)')
+
+    @classmethod
+    def __register__(cls, module_name):
+        pool = Pool()
+        Module = pool.get('ir.module')
+
+        cursor = Transaction().cursor
+        module = Module.__table__()
+
+        cursor.execute(*module.update(
+                columns=[module.state],
+                values=[Literal('to remove')],
+                where=module.name == Literal('product_configuration')
+                ))
+
+        super(Configuration, cls).__register__(module_name)
 
     @staticmethod
     def default_thumb_size():
